f1ef171c0eccb2c4b2a058f8a1186994fd03377c
Neha Sen <nsen@attosol.com>
2022-11-18 23:48:14 +0530

    ok

--- badal-sen-portfolio/src/Components/about/about.js
+++ badal-sen-portfolio/src/Components/about/about.js
@@ -1,12 +1,31 @@
-import background from "../images/background-2.webp"
+import background from "../images/background.jpeg"
+import badal from "../images/badal.jpeg"
 import "./about.css";
+import {useEffect, useState} from "react";
 
 export const About = (() => {
+    const {slideCount, setSlideCount} = useState(0) 
+
+    useEffect({
+        
+
+    }[slideCount])
 
     return(
         <div className="about">
-            <div>
-                <img src={background} className="bac-img" />
+            <div className='carousel'>
+                <div className='slide '>
+                    <h1>Hi, this is slide 1</h1>
+                </div>
+                <div className='slide none'>
+                    <h1>Hi, this is slide 2</h1>
+                </div>
+                <div className='slide none'>
+                    <h1>Hi, this is slide 3</h1>
+                </div>
+                <div className='slide none'>
+                    <h1>Hi, this is slide 4</h1>
+                </div>
             </div>
         </div>
     )
--- badal-sen-portfolio/src/Components/navbar/navbar.js
+++ badal-sen-portfolio/src/Components/navbar/navbar.js
@@ -1,22 +1,26 @@
 import "./navbar.css";
 
-export const Navbar = (() => {
-
-    return (
-        <nav>
-            <div className="logo">BSen</div>
-            <div className="container">
-                <button className="item">About
-                    <div className="under"></div>
-                </button>
-                <button className="item">Exprtise
-                    <div className="under"></div>
-                </button>
-                <button className="item">Contact Me
-                    <div className="under"></div>
-                </button>
-            </div>
-
-        </nav>
-    )
-}) 
+export const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div className="logo">BSen</div>
+        <div className="container">
+          <button className="item">
+            About
+            <div className="under"></div>
+          </button>
+          <button className="item">
+            Exprtise
+            <div className="under"></div>
+          </button>
+          <button className="item">
+            Contact Me
+            <div className="under"></div>
+          </button>
+        </div>
+      </nav>
+      <div className="spacediv"></div>
+    </>
+  );
+};
